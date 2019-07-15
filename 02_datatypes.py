c6973f9804d1de637f12b5ac511049111370ccea
Payal Nayee <PNayee@spartaglobal.com>
2019-07-15 13:51:59 +0100

    updated exercises added

--- 02_datatypes.py
+++ 02_datatypes.py
@@ -6,62 +6,62 @@
 # List of Characters bundled together in a specific order
 #Using Index
 
-# print('hello')
-# print(type('hello'))
-#
-# #Concatenation of Strings - joining of two strings
-#
-# string_a = 'hello there'
-# name_person = 'Juan Pier'
-#
-# print (string_a + ' ' + name_person)
-#
-# #Useful methods
-# #Length
-# print (len(string_a))
-# print (len(name_person))
-#
-# #Strip = Removes trailing white spaces
-#
-# string_num = ' 90323 '
-# print(type(string_num))
-# print(string_num)
-# print(string_num.strip())
-#
-# #.split - a method for strings
-# #It splits in a specific location and output a list (data type)
-#
-# string_text = 'Hello I need to go to the loo'
-# split_string =string_text.split(' ')
-# print(split_string)
-#
-# #Capturing User Input -  capture and display user input
-#
-# #user_input_first_name = input('What is your first name')
-# #print (user_input_first_name)
-#
-# # get user input and print first and last name
-# # 1) get user input/ first name
-# # save user input to variable
-# first_name = input ('What is your first name')
-# # get user last name
-# # and save it to variable
-# last_name  = input ('what is your last name?')
-#
-# #user_input_last_name = input('What is your last name')
-# #print (user_input_last_name)
-#
-#
-# # join two and
-# # Let us use concactenation
-# # Let us use interpolation
-# # print
+print('hello')
+print(type('hello'))
 
-# full_name = first_name +' ' +last_name
-# print (full_name)
-# #Let us use interpolation
-# welcome_message  = f"Hi {full_name} you are very welcome!"
-# print(welcome_message)
+#Concatenation of Strings - joining of two strings
+
+string_a = 'hello there'
+name_person = 'Juan Pier'
+
+print (string_a + ' ' + name_person)
+
+#Useful methods
+#Length
+print (len(string_a))
+print (len(name_person))
+
+#Strip = Removes trailing white spaces
+
+string_num = ' 90323 '
+print(type(string_num))
+print(string_num)
+print(string_num.strip())
+
+#.split - a method for strings
+#It splits in a specific location and output a list (data type)
+
+string_text = 'Hello I need to go to the loo'
+split_string =string_text.split(' ')
+print(split_string)
+
+#Capturing User Input -  capture and display user input
+
+#user_input_first_name = input('What is your first name')
+#print (user_input_first_name)
+
+# get user input and print first and last name
+# 1) get user input/ first name
+# save user input to variable
+first_name = input ('What is your first name')
+# get user last name
+# and save it to variable
+last_name  = input ('what is your last name?')
+
+#user_input_last_name = input('What is your last name')
+#print (user_input_last_name)
+
+
+# join two and
+# Let us use concactenation
+# Let us use interpolation
+# print
+
+full_name = first_name +' ' +last_name
+print (full_name)
+#Let us use interpolation
+welcome_message  = f"Hi {full_name} you are very welcome!"
+print(welcome_message)
 
 # Count /lower/ upper/ capitalize
 text_example = "Here is some text, with lot's of text"
---
+++ 05_python_class_exercise.py
@@ -0,0 +1,19 @@
+# Python Exercise for Practise
+#This is the file where I will do my exercise
+#Filipe told me to start with Pseudo code
+
+#Get User first Name
+# save to variable
+# get user
+
+full_name  = input ('What is your name?')
+
+important_date = input ('What is your most important date?')
+
+reason_date= input ('Why is that date most important?')
+
+funny_fact = input ('What is your funny fact?')
+
+favourite_pokemon= input ('Who is your favourite Pokemon?')
+
+
