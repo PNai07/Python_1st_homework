aa5fa60972560c58c93d20f37188a8e3911a87b2
Payal Nayee <PNayee@spartaglobal.com>
2019-07-15 11:19:21 +0100

    Completed strings

---
+++ 01Variables.py
@@ -0,0 +1,16 @@
+#Variables in Python
+
+#Variable is like a box
+#Has a name
+#Stores things inside
+
+my_variable = 'Hello there'
+
+
+# Variable with a string being printed to console
+print(my_variable)
+
+#Reassigning variable
+
+my_variable = "I'm new content being reassigned to my_variable"  + 'more things'
+print(my_variable)
