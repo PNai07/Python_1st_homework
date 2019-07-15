5bc049588940cbb4ec70025acb267180635aa1a8
Payal Nayee <PNayee@spartaglobal.com>
2019-07-15 16:56:01 +0100

    class exercise task updated

---
+++ 15_07_class_exercise2.py
@@ -0,0 +1,10 @@
+# Create a list with 10 potential clients
+
+sparta_clients= { 'sparta_clients': ['Direct Line', 'RBS', 'Rakuten','ASOS','Santander', 'Earthport','Nike', 'National Rail', 'Home Office', 'Ministry of Justice']}
+
+
+nice_message =  f"These potential {sparta_clients} are amazing with many offices across the globe and have great enthusiasm!"
+print(nice_message)
+
+
+sparta_clients = '10'
