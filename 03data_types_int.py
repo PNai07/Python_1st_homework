6c15806a6783a2e616ecc958cef232e6d95e33cf
Payal Nayee <PNayee@spartaglobal.com>
2019-07-15 12:01:55 +0100

    datatypes for integers complete

---
+++ 03data_types_int.py
@@ -0,0 +1,61 @@
+# Numericals  - Integers, long, complex numbers
+
+# Integers Whole Numbers
+my_int = 13
+print(type(13)) #Not the same as print(type('13'))--> STRING!
+
+#Floats and composite numbers
+print(13.1)
+print (type(13.1))
+
+# Long and big complex numbers
+# Less used check syntax when needed
+
+#Operators
+#Add
+num_a = 10
+num_b = 7.3
+print(num_a + num_b)
+print(num_a - num_b)
+print(num_b * 10)
+print(num_a / 2)
+
+#Modulus - Check for remainders
+
+print (10 % 2)
+#Checking if even
+print((12%2) == 0)
+print (18 % 3)
+
+#Logical Operators
+#The evaluate two sides and return a boolean
+#Check if equal ==
+a = 10
+b = 13
+print (a==b)
+print (a==10)
+
+#Checking if bigger and smaller than
+print(a>b)
+print(a>9)
+print('....')
+
+print (b<a)
+print (a<15)
+
+#Checking bigger/smaller or equal
+print (a > 10)
+print (a>= 10)
+
+#Check if not something
+print (a!= 9) # a is not 9, it is 10, so it returns true
+
+#Increment add something to something
+counter = 0
+counter +=1
+print(counter)
+counter +=1
+print(counter)
+
+
+
