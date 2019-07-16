bc78585a4a96a0a0b03c4b137b9e7a73e55d11e8
Payal Nayee <PNayee@spartaglobal.com>
2019-07-16 16:35:29 +0100

    functions part one altered

---
+++ 13_functions.py
@@ -0,0 +1,36 @@
+# Functions
+# Run blocks of code when called
+# They should be unitary / small
+# They can take in argumets
+
+# function is like a machine
+# it can take in inputs
+# it can perform actions
+# it can output different objects
+
+#An argument in a function is a variable that exists only in the scop of the function
+#syntax
+
+#def name_function(arg1, arg2):
+#runs blocks of code
+
+
+
+def full_name(f_name, l_name):
+  return f_name + ' ' + l_name
+
+
+result_function = full_name('Hamza', 'Danielson')
+print(result_function)
+
+
+# These arguments can only be used inside the block of code
+
+
+# Good practices
+# - functions should be unitary/ small jobs/ one job
+# - arguments should have good names
+# - functions should be used to keep the oode DRY
+# - DRY - Don't repeat yourself
+# -
+
