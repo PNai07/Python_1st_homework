cc86214d84b79e0d45ff8e009c67c800eae43bff
Payal Nayee <PNayee@spartaglobal.com>
2019-07-16 09:37:59 +0100

    python homework updated

---
+++ Homework_exercise.py
@@ -0,0 +1,15 @@
+print ('-----------------------------------------')
+
+superhero_name = input ('What is your superhero name? ')
+story = {
+'start': 'Once upon a time there was a superhero called:  ',
+'superhero': superhero_name,
+'middle':'this superhero had all the power in the world.',
+'end': 'This (superhero) was a real life saver as he always did good for the world and, one of his special deeds was to save all the bunnies in danger.'
+
+}
+
+print(story['start'], story['superhero']),
+print(story['middle']),
+print(story['end']),
+# prints ('keys')
